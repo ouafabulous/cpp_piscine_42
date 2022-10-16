34ac8cc92c86b3e46fe9cfdd76355f99640d2900
Ouafae MOUDNI <omoudni@e1r12p4.clusters.42paris.fr>
2022-10-17 00:29:03 +0200

    adavanced in ex01

--- d00/ex01/main.cpp
+++ d00/ex01/main.cpp
@@ -3,22 +3,24 @@
 #include <sstream>
 #include <string>
 
-int	main( void ) {
-
-std::string			opt1 = "ADD";
-std::string			opt2 = "SEARCH";
-std::string			opt3 = "EXIT";
-std::string			cmd;
-std::string			inputs[5];
-std::string			id_txt;
-int					id;
-bool				is_ok;
-PhoneBook			repertoire;
-
-std::cout << "Choose an option amongst the following: ADD, SEARCH, EXIT: ";
-while (std::getline(std::cin, cmd) && cmd != opt3)
+int main(void)
 {
-	if (cmd == opt1)
+
+	std::string opt1 = "ADD";
+	std::string opt2 = "SEARCH";
+	std::string opt3 = "EXIT";
+	std::string cmd;
+	std::string inputs[5];
+	std::string id_txt;
+	int id;
+	bool is_ok;
+	PhoneBook repertoire;
+
+	while (1)
+	{
+		std::cout << "Choose an option amongst the following: ADD, SEARCH, EXIT: ";
+		std::getline(std::cin, cmd);
+		if (cmd == opt1)
 		{
 			std::cout << "First name: ";
 			std::cin >> inputs[0];
@@ -32,26 +34,30 @@ while (std::getline(std::cin, cmd) && cmd != opt3)
 			std::cin >> inputs[4];
 			repertoire.fill_contact(inputs);
 		}
-	else if (cmd == opt2)
-	{
-		is_ok = false;
-		while (!is_ok)
-		{
-		std::cout<< "Enter the contact Id (in range 0-7): ";
-		std::cin >> id_txt;
-		try {
-			std::istringstream(id_txt) >> id;
-			if (id >= 0 && id <= 7)
-				is_ok = true;
-		}
-		catch (std::exception &err)
+		else if (cmd == opt2)
 		{
 			is_ok = false;
+			while (!is_ok)
+			{
+				std::cout << "Enter the contact Id (in range 0-7): ";
+				std::cin >> id_txt;
+				try
+				{
+					std::istringstream(id_txt) >> id;
+					if (id >= 0 && id <= 7)
+						is_ok = true;
+				}
+				catch (std::exception &err)
+				{
+					is_ok = false;
+				}
+			}
+			repertoire.print_contact(id);
 		}
+		else if (cmd == opt3)
+		{
+			break;
 		}
-		repertoire.print_contact(id);
 	}
-//	std::cout << "Choose an option amongst the following: ADD, SEARCH, EXIT: ";
-}
 	return (0);
 }
