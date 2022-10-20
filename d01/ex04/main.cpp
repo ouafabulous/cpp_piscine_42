43a971806aae68051e73d7187ec89c1acecb1f3b
Ouafae MOUDNI <omoudni@e3r3p15.clusters.42paris.fr>
2022-10-20 20:27:20 +0200

    d01-ex04 done

--- d01/ex04/main.cpp
+++ d01/ex04/main.cpp
@@ -6,46 +6,49 @@
 
 int main(int ac, char **av)
 {
-	int		i;
-	int		len_s1;
-	char	c;
 
 	if (ac == 4)
 	{
 		std::ifstream		ifs(av[1]);
-		std::string			new_av1(av[1]);
-		std::string			new_av2(av[2]);
-		std::string			new_av3(av[3]);
-		std::stringstream	to_copy_stream;
-		std::string			to_copy;
+		std::string			infile_name(av[1]);
+		std::string			outfile_name = infile_name + ".replace";
+		std::string			to_replace(av[2]);
+		std::string			replace_by(av[3]);
+		std::stringstream	buffer;
+		std::string			file_content;
+		int					index;
 
-		len_s1 = new_av2.length();
 		if (ifs.fail()) {
-			std::cout << "File Error" << std::endl << "1- Check if the file exists Does" << std::endl << "2- Check its permissions" << std::endl;
+			std::cout << "File Error." << std::endl << "1- Check if the file exists." << std::endl << "2- Check its permissions." << std::endl;
 			return (1);
 		}
-		new_av1 += ".replace";
-		std::ofstream	ofs(new_av1.c_str());
+
+		if (to_replace == "") {
+			std::cout << "substring to replace shouldn't be an empty string!" << std::endl;
+			return (1);
+		}
+
+		std::ofstream		ofs(outfile_name.c_str());
+
 		if (ofs.fail()) {
 			std::cout << "Couldn't create <output_file>" << std::endl;
 			return (1);
 		}
-		while (1) {
-			i = 0;
-			while (i < len_s1 - 1) {
-				to_copy_stream << std::flush;
-				if (!ifs.get(c))
-				{
-					ofs << to_copy_stream.rdbuf();
-					exit (0);
-				}
-				to_copy_stream << c;
-				i++;
+
+		buffer << ifs.rdbuf();
+		file_content = buffer.str();
+
+		while (true) {
+			index = file_content.find(to_replace);
+			if (index < 0) {
+				break;
 			}
-			if (!new_av2.compare(to_copy_stream.str())) {
-				ofs << new_av3;
-			}
+			file_content.erase(index, to_replace.length());
+			file_content.insert(index, replace_by);
 		}
+
+		ofs << file_content;
+
 	}
 	else
 		std::cout << "Usage: ./exe <filename> str1 str2" << std::endl;
