42f94b27faa8ab508fc05415edea4b8ba208a5e4
OUAFABULOUS <ouafae1310@gmail.com>
2023-04-01 19:22:26 +0200

    d09-ex00 input regex done

--- d09/ex00/main.cpp
+++ d09/ex00/main.cpp
@@ -9,15 +9,25 @@ int regex_match(const char *string)
     // reti = regcomp(&regex, "^(19|20)\\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\\d|3[0-1])\\s\\|\\s[+]?([0-9]+([.][0-9]*)?|[.][0-9]+)$", 0);
 
 	// reti = regcomp(&regex, "^(19|20)\\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[1-2]\\d|3[0-1])$", 0);
-	reti = regcomp(&regex, "^(19|20)\\d{2}$", 0);
-    if (reti)
+	// std::cout << "The string I'm handling: " << string << std::endl;
+	reti = regcomp(&regex, "^\\(19\\|20\\)[0-9][0-9]-\\(0[1-9]\\|1[0-2]\\)-\\(0[1-9]\\|1[0-9]\\|2[0-9]\\|3[0-1]\\)\\s|\\s\\([0-9]\\+\\([.][0-9]\\+\\)\\?\\|[.][0-9]\\+\\)$", 0);
+    // reti = regcomp(&regex, "^\\(19\\|20\\)[0-9][0-9]$", 0);
+	if (reti)
 		throw std::runtime_error("Internal error");
 
     /* Execute regular expression */
-    reti = regexec(&regex, string, 0, NULL, REG_EXTENDED);
+    reti = regexec(&regex, string, 0, NULL, 0);
     if (!reti)
+	{
+		std::cout << "Match" << std::endl;
 		return 0;
-    else
+	}
+    else if (reti == REG_NOMATCH)
+	{
+		std::cout << "No match" << std::endl;
+		return 1;
+	}
+	else
 		throw std::runtime_error("Invalid date");
 	regfree(&regex);
     return 0;
